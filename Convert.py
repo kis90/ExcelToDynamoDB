25ed7d765673661638bf7c4848c5803e7defffc2
kis90 <69447904+kis90@users.noreply.github.com>
2020-10-15 12:38:16 +0530

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -1,16 +1,16 @@
-import openpyxl
-filename = 'Dynamic.xlsx'
-xlsx = openpyxl.load_workbook(filename)
-sheet = xlsx.active
-data = sheet.rows
-csv = open("data.csv", "w+")
+import xlrd
+import csv
 
-for row in data:
-    l = list(row)
-    for i in range(len(l)):
-        if i == len(l) - 1:
-            csv.write(str(l[i].value))
-        else:
-            csv.write(str(l[i].value) + ',')
-    csv.write('\n')
-csv.close()
+def csv_from_excel():
+    wb = xlrd.open_workbook('Dynamic.xlsx')
+    sh = wb.sheet_by_name('Sheet1')
+    your_csv_file = open('data1.csv', 'w')
+    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    your_csv_file.close()
+
+# runs the csv_from_excel function:
+csv_from_excel()
